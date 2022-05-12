e6a11cfa8d972b957c07033960ca9df91586ab57
Gabriel Ramos <gabrielramos.blp@gmail.com>
2022-05-12 18:43:26 -0300

    para fixar sobre variáveis

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-4.1/teste.js
@@ -0,0 +1,15 @@
+const myName = 'Gabriel'
+const birthCity = 'BH'
+let birthYear = '2004'
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030
+
+console.log(birthYear);
+
+birthCity = 'Brumadinho'
+
+console.log(birthYear);
