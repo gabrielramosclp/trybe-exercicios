fae66178054d6d3f2b75eff941dfc9136db1ffea
Gabriel Ramos <gabrielramos.blp@gmail.com>
2022-05-15 20:05:22 -0300

    faz exercícios de 4 ao 8, sobre condicionais, comparativos e aritméticos

--- fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-4.1/exercicio-dia-4-1.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-4.1/exercicio-dia-4-1.js
@@ -1,3 +1,4 @@
+/* exercício 1 */
 const a = 10
 const b = 20
 
@@ -7,6 +8,8 @@ console.log(a * b)
 console.log(a / b)
 console.log(a % b)
 
+
+/* exercício 2 */
 const number1 = 20
 const number2 = 30
 const number3 = 40
@@ -17,10 +20,60 @@ if (number1 > number2) {
     console.log(number2);
 }
 
+
+/* exercício 3 */
 if (number1 > number2 && number1 > number3) {
     console.log(number1);
 } else if (number2 > number1 && number2 > number3) {
     console.log(number2);
 }else {
     console.log(number3);
+}
+
+
+/* exercício 4 */
+const negativo = -20
+
+if (negativo > 0) {
+    console.log('positive');
+}else if (negativo < 0) {
+    console.log('negative');
+}else {
+console.log('zero');
+}
+
+
+/* exercício 5 */
+const angulo1 = 60
+const angulo2 = 30
+const angulo3 = 90
+
+if (angulo1 + angulo2 + angulo3 === 180) {
+    console.log(true);
+} else if (angulo1 + angulo2 + angulo3 != 180){
+    console.log(false);
+}else {
+    console.log('erro');
+}
+
+
+/* exercício 6 */
+const pecaXadrez = 'bishop'
+
+if (pecaXadrez === 'bishop') {
+    console.log('diagonals');
+} else {
+    console.log('erro');
+}
+
+
+/* exercício 8 */
+const par1 = 10
+const par2 = 50
+const impar = 23
+
+if (par1 %2 === 0 | par2 %2 === 0 | impar %2 === 0) {
+    console.log(true);
+} else {
+    console.log(false);
 }
