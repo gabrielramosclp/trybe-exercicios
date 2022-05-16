1d94e3df146a618158303662fb1b694fc7083587
Gabriel Ramos <gabrielramos.blp@gmail.com>
2022-05-16 20:22:52 -0300

    Exercício 2, imprimir a string tryber invertida

--- fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-4.3/exercicios-dia-4-3.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-4.3/exercicios-dia-4-3.js
@@ -9,4 +9,14 @@ for(index = 10; index > 1; index -= 1) {
 }
     console.log(resultado);
     
-    
+/* 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente. */
+
+let word = 'tryber'
+let reverse = ''
+let stringReverse = []
+
+for (index = word.length-1; index >= 0; index -= 1) {
+    reverse = word[index]
+    stringReverse.push(reverse)
+}
+console.log(stringReverse);    
